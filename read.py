66f33b46b4ebcff29ce6cde6ae22bba6e4561ea8
Eddie Zhang <zhangzian1996@gmail.com>
2018-08-16 14:29:17 +0800

    add filter

--- read.py
+++ read.py
@@ -12,3 +12,11 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('每份留言的平均长度为：', sum_len/ len(data))
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '份留言长度小于100')
+print(new[1])
